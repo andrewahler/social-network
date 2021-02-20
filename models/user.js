6eab73a891f429024b53c1826bd12e1664709e45
Andrew Ahler <andrewahler@gmail.com>
2021-02-20 14:16:06 -0600

    updated models

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Email validation failed'
+              }
+        },
+
+
+
+
+
+})
+
+
+UserSchema
