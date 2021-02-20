8251906cb3b81f874fa319b8060a29c0f3cb4eef
Andrew Ahler <andrewahler@gmail.com>
2021-02-20 13:48:26 -0600

    added utils/models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Email = require('./Email');
+const Friends = require('./Friends');
+const Thoughts = require('./Thoughts');
+const User = require('./User');
+
+module.exports = {Email, Friends, Thoughts, User}
